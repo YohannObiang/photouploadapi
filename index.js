8c8018059bc30205360ef940370d4dec90226456
Yohann Obiang <104835751+YohannObiang@users.noreply.github.com>
2022-11-28 17:21:41 +0100

    28/11 17:21

--- index.js
+++ index.js
@@ -38,34 +38,34 @@ app.use(express.json())
 app.use(cors())
 app.use(errHandler);
 
-// const con = mysql.createPool({
-//     connectionLimit : 100,
-//     waitForConnections : true,
-//     queueLimit :0,
-//     host     : 'db4free.net',
-//     user     : 'yohannobiang',
-//     password : '@Bolo1997',
-//     database : 'obisto',
-//     debug    :  true,
-//     wait_timeout : 28800,
-//     connect_timeout :10
-// });
-app.use('/uploads', express.static('uploads'));
-app.use('/bg', express.static('bg'));
-
-
 const con = mysql.createPool({
     connectionLimit : 100,
     waitForConnections : true,
     queueLimit :0,
-    host     : 'localhost',
-    user     : 'root',
-    password : '',
+    host     : 'db4free.net',
+    user     : 'yohannobiang',
+    password : '@Bolo1997',
     database : 'obisto',
     debug    :  true,
     wait_timeout : 28800,
     connect_timeout :10
 });
+app.use('/uploads', express.static('uploads'));
+app.use('/bg', express.static('bg'));
+
+
+// const con = mysql.createPool({
+//     connectionLimit : 100,
+//     waitForConnections : true,
+//     queueLimit :0,
+//     host     : 'localhost',
+//     user     : 'root',
+//     password : '',
+//     database : 'obisto',
+//     debug    :  true,
+//     wait_timeout : 28800,
+//     connect_timeout :10
+// });
 
 
 
